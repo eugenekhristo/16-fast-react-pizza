247896f3ca4d9d4eee175428064553b167356087
Eugene Khristo <marteau9292@gmail.com>
2025-04-24 22:35:57 +0300

    Tailwind some more

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,7 @@
 function UserName() {
-  return <p className="text-sm font-semibold">Eugene Khristo</p>;
+  return (
+    <p className="hidden text-sm font-semibold md:block">Eugene Khristo</p>
+  );
 }
 
 export default UserName;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,21 +1,23 @@
-import Header from './Header';
-import CartOverview from '../features/cart/CartOverview';
-import { Outlet, useNavigation } from 'react-router-dom';
-import Loader from './Loader';
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === 'loading';
+  const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       <Header />
 
-      <main>
-        {isLoading && <Loader />}
+      <div>
+        <main className="mx-auto max-w-3xl">
+          {isLoading && <Loader />}
 
-        <Outlet />
-      </main>
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
